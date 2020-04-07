caaf9b873a86a128b4b55314fe704a4a25e8db95
AntonAnton <tohatosha@googlemail.com>
2020-04-07 16:36:44 +0300

    first test

--- run.py
+++ run.py
@@ -1,1 +1,20 @@
+import falcon
 
+
+class Resource(object):
+    def on_get(self, req, resp):
+        """Handles GET requests"""
+        resp.status = falcon.HTTP_200  # This is the default status
+        resp.body = ('\nTwo things awe me most, the starry sky '
+                     'above me and the moral law within me.\n'
+                     '\n'
+                     '    ~ Immanuel Kant\n\n')
+
+# falcon.API instances are callable WSGI apps
+app = falcon.API()
+
+# Resources are represented by long-lived class instances
+resource = ThingsResource()
+
+# things will handle all requests to the '/things' URL path
+app.add_route('/resource', resource)
